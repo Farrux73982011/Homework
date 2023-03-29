6804ad1f35383f4fedda78e1334048fc3693d1db
Developer <112705739+Farrux73982011@users.noreply.github.com>
2023-03-29 19:56:52 +0500

    Web create

---
+++ JS4/js/index.js
@@ -0,0 +1,66 @@
+// let dakata = +prompt('Day').trim();
+
+// if(dakata >= 1 &&dakata <= 10){
+//     alert('1 deketa')
+// }else if(dakata >= 11 && dakata <= 19){
+//     alert('2 dekata')
+// }else if(dakata >= 20 && dakata <= 31){
+//     alert('3 dekata')
+// }
+
+// let name_ = prompt('Как вас зовут?').trim().toUpperCase;
+
+// if(name_ === 'Alex'){
+//     alert('Пользователь найден')
+// }else{
+//     alert('Пользователь ненайден')
+// }
+
+
+
+// let account_ = prompt('Номер счёта?').trim();
+
+// if(account_ === '7777'){
+//     alert('Всё правильно')
+// }else{
+//     alert('Счёт неправильный')
+// }
+
+
+
+// let money_2 = prompt('Сколько обналичить?').trim();
+
+// if(money_2 <= 10000){
+//     confirm('Всё отлично')
+//     alert( 10000 - money_2)
+//     alert(money_2)
+// }else{
+//     alert('Не достаточно средств')
+// }
+
+
+// let name_2 = prompt('Ваше имя?').slice(0, 1).toLowerCase;
+// let age_ = prompt('Сколько лет?');
+// let money_3 = prompt('Сколько у вас денег?');
+// let commanda = prompt('С вами сколько людей');
+
+// if(name_2 === "а" ){
+//     if(age_ >= 20 && age_ <= 40){
+//         if(money_3 >= 100){
+//             if(commanda <= 10){
+//                 alert('Welcome')
+//             }
+//             else{
+//                 alert('Иди')
+//             }
+//         }
+//         else{
+//             alert('Иди')
+//         }
+//     }
+//     else{
+//         alert('Иди')
+//     }
+// }else{
+//     alert('Иди отсюда')
+// }
