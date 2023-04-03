20341b04ee7e53fd750bac9a9acba99d0bc5b05c
Developer <112705739+Farrux73982011@users.noreply.github.com>
2023-04-03 18:47:09 +0500

    site ready

--- JS6/js/insex.js
+++ JS6/js/insex.js
@@ -1,24 +1,24 @@
-let arr = ["morgen", "nikolay", "alexandr", "yulduz", "timati"];
+// let arr = ["morgen", "nikolay", "alexandr", "yulduz", "timati"];
 
-let numbers = [2, 4, 1, 4, 7, 3, 2, 7, 9, 3, 2];
+// let numbers = [2, 4, 1, 4, 7, 3, 2, 7, 9, 3, 2];
 
-let even = [];
-let odd = [];
-let flt = numbers.filter((item) => {
-  if (item % 2 === 0) {
-    even.push(item);
-  } else {
-    odd.push(item);
-  }
-});
+// let even = [];
+// let odd = [];
+// let flt = numbers.filter((item) => {
+//   if (item % 2 === 0) {
+//     even.push(item);
+//   } else {
+//     odd.push(item);
+//   }
+// });
 
-console.log(even);
-console.log(odd);
+// console.log(even);
+// console.log(odd);
 
-let max_length = arr[0];
-for (let i of arr) {
-  if (max_length.length < i.length) {
-    max_length = i;
-  }
-}
-console.log(max_length);
+// let max_length = arr[0];
+// for (let i of arr) {
+//   if (max_length.length < i.length) {
+//     max_length = i;
+//   }
+// }
+// console.log(max_length);
