5b25b200a38b8271ddfc292465920e18f69da6b0
Developer <112705739+Farrux73982011@users.noreply.github.com>
2023-04-17 19:41:11 +0500

    site ready

---
+++ JS11/js/index.js
@@ -0,0 +1,19 @@
+let i = 9;
+let parol = 7777;
+let p = prompt('Пароль');
+
+do{
+  if(p !== parol){
+    p =+prompt(`У вас ещё ${i} попытки`);
+  }
+  else if(p == parol , i == 9){
+    prompt('Добро пожаловать!');
+    
+  }
+ else if (0){
+    prompt("Попробуйте позже!");   
+  }
+i++;
+}while(
+  i>0
+)
