f935c816ffe6356e8a662beb1e995c9511351eab
Developer <112705739+Farrux73982011@users.noreply.github.com>
2023-04-24 19:48:11 +0500

    site ready

--- JS14/js/index.js
+++ JS14/js/index.js
@@ -1,6 +1,6 @@
 let pro = +prompt('Сколько раз хотите складывать что-то?')
 let fibonacci = [0, 1];
 for (let i = 2; i <= pro; i++) {
-    fibonacci[i - 2] + fibonacci[i - 1]
+    fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
 }
 console.log(fibonacci);
